ea72ebca2a1ea593c75d4e803313a5e8f5933499
Yhyun31 <younghyun1954@naver.com>
2025-03-13 13:49:08 +0900

    feat: 헤더

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import styled from "styled-components/native";
+
+const HeaderContainer = styled(View)`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 40px;
+  position: relative;
+  height:50px;
+`;
+
+const HeaderTitle = styled(Text)`
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+  flex: 1;
+ 
+`;
+
+const Header = ({title}) => {
+    const navigation = useNavigation();
+  
+    return (
+      <HeaderContainer>
+        <TouchableOpacity onPress={() => navigation.goBack()} >
+          <Feather name="arrow-left" size={24} color="black" />
+        </TouchableOpacity>
+        <HeaderTitle>{title}</HeaderTitle>
+      </HeaderContainer>
+    );
+  };
+  
+  export default Header;
