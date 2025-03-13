3d42d5fe1efb9e58eb44d7eeb38b24ccee491f33
Yhyun31 <younghyun1954@naver.com>
2025-03-13 16:30:54 +0900

    feat: 마이페이지

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Auth from "./Auth";
-import Main from "./Main";
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Main />
+      <Auth />
     </NavigationContainer>
   );
 };
