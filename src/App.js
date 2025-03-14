76d98a476a7d08f5f6d99994e5f3eb6606a3449b
Yhyun31 <younghyun1954@naver.com>
2025-03-14 15:06:55 +0900

    ✨feat: 폰트적용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
-import theme from "./theme";
-import { StatusBar } from "expo-status-bar";
-import Navigation from "./navigations";
-
-const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <StatusBar style="auto" />
-      <Navigation />
-    </ThemeProvider>
-  );
-};
-
-export default App;
+ import { ThemeProvider } from "styled-components";
+ import theme from "./theme";
+ import { StatusBar } from "expo-status-bar";
+ import Navigation from "./navigations";
+ import * as Font from "expo-font";
+ import { useFonts } from "expo-font";
+ import AppLoading from "expo-app-loading";
+ 
+ const loadFonts = () => {
+   return Font.loadAsync({
+     NanumSquare_acB: require("../assets/fonts/NanumSquare_acB.ttf"),
+     NanumSquare_acEB: require("../assets/fonts/NanumSquare_acEB.ttf"),
+     NanumSquare_acR: require("../assets/fonts/NanumSquare_acR.ttf"),
+   });
+ };
+ 
+ const App = () => {
+   const [fontsLoaded] = useFonts({
+     NanumSquare_acB: require("../assets/fonts/NanumSquare_acB.ttf"),
+     NanumSquare_acEB: require("../assets/fonts/NanumSquare_acEB.ttf"),
+     NanumSquare_acR: require("../assets/fonts/NanumSquare_acR.ttf"),
+   });
+ 
+   if (!fontsLoaded) {
+     return <AppLoading />;
+   }
+   return (
+     <ThemeProvider theme={theme}>
+       <StatusBar style="auto" />
+       <Navigation />
+     </ThemeProvider>
+   );
+ };
+ 
+ export default App;
