9148790ea80b04515d0ae9edf984c67f23f7d81b
syuzzzi <csj1430@naver.com>
2025-03-25 16:46:06 +0900

    패키지 맞추기

--- src/screens/index.js
+++ src/screens/index.js
@@ -6,12 +6,26 @@ import SignupDone from "./SignupDone";
 import Chat from "./Chat";
 import MainPage from "./MainPage";
 import Notifications from "./Notifications";
-import MyPage from './MyPage';
-import Search from './Search';
+import MyPage from "./MyPage";
+import Search from "./Search";
 import AllPosts from "./AllPosts";
 import Profile from "./Profile";
 import EditProfile from "./EditProfile";
 import CreatePost from "./CreatePost";
 
-export { Signin, SigninWithEmail, Signup, FindPw, SignupDone,Chat,MainPage,Notifications,Search,AllPosts,MyPage,Profile,EditProfile,CreatePost};
-
+export {
+  Signin,
+  SigninWithEmail,
+  Signup,
+  FindPw,
+  SignupDone,
+  Chat,
+  MainPage,
+  Notifications,
+  Search,
+  AllPosts,
+  MyPage,
+  Profile,
+  EditProfile,
+  CreatePost,
+};
