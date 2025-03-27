8d7249dbd511eebdc6af80774975d0fc14212685
syuzzzi <csj1430@naver.com>
2025-03-27 18:56:10 +0900

    ✨feat : 신청서 확인 화면 구현

--- src/screens/ApplicationDecision.js
+++ src/screens/ApplicationDecision.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { Input, Button } from "../components";
-import styled from "styled-components/native";
+import React, { useContext } from "react";
+import { Button } from "../components";
+import styled, { ThemeContext } from "styled-components/native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const Container = styled.View`
@@ -11,43 +11,87 @@ const Container = styled.View`
   padding: 0 30px;
   padding-bottom: ${({ insets: { bottom } }) => bottom}px;
 `;
+const ProfileContainer = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  margin-top: 30px;
+  margin-bottom: 10px;
+`;
+const ProfileImage = styled.Image`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  margin-right: 20px;
+  margin-bottom: 20px;
+`;
+const NameText = styled.Text`
+  font-size: 16px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+  color: ${({ theme }) => theme.colors.black};
+  margin-bottom: 20px;
+`;
+const Form = styled.Text`
+  width: 100%;
+  font-size: 15px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  justify-content: flex-start;
+  align-items: flex-start;
+  margin-bottom: 30px;
+  line-height: 23px;
+`;
+const ButtonContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
 
-const ApplicationDecision = ({ navigation }) => {
+const ApplicationDecision = ({ route, navigation }) => {
   const insets = useSafeAreaInsets();
+  const theme = useContext(ThemeContext);
 
-  const [form, setForm] = useState("");
-  const [disabled, setDisabled] = useState(true);
-
-  useEffect(() => {
-    setDisabled(form.trim().length === 0);
-  }, [form]);
+  const { name, image } = route.params;
 
   return (
     <Container insets={insets}>
-      <Input
-        returnKeyType="done"
-        value={form}
-        onChangeText={(text) => setForm(text)}
-        placeholder="자기소개 및 지원동기"
-        containerStyle={{ width: "100%" }}
-        textStyle={{ height: 230 }}
-        multiline={true}
-        numberOfLines={10}
-      />
-      <Button
-        title="제출"
-        onPress={() => console.log("제출")}
-        disabled={disabled}
-        containerStyle={{
-          width: 82,
-          height: 35,
-          marginTop: 20,
-          marginBottom: 0,
-          paddingTop: 0,
-          paddingBottom: 0,
-        }}
-        textStyle={{ marginLeft: 0, fontSize: 16 }}
-      />
+      <ProfileContainer>
+        <ProfileImage source={image} />
+        <NameText>{name}</NameText>
+      </ProfileContainer>
+      <Form>
+        안녕하세요 저는 {name}입니다.{"\n"}쉬는 날 뜨개질을 하며 쉬는 게 취미인데{"\n"}다른 분들과 함께 공유하며 하고 싶어서{"\n"}신청했어요!
+      </Form>
+
+      <ButtonContainer>
+        <Button
+          title="수락"
+          onPress={() => console.log("수락")}
+          containerStyle={{
+            width: 82,
+            height: 35,
+            marginTop: 20,
+            marginBottom: 0,
+            marginRight: 50,
+            paddingTop: 0,
+            paddingBottom: 0,
+          }}
+          textStyle={{ marginLeft: 0, fontSize: 16 }}
+        />
+        <Button
+          title="거절"
+          onPress={() => console.log("거절")}
+          containerStyle={{
+            width: 82,
+            height: 35,
+            marginTop: 20,
+            marginBottom: 0,
+            paddingTop: 0,
+            paddingBottom: 0,
+            backgroundColor: theme.colors.lightBlue,
+          }}
+          textStyle={{ marginLeft: 0, fontSize: 16, color: theme.colors.black }}
+        />
+      </ButtonContainer>
     </Container>
   );
 };
